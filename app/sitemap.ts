a5b4d3af8bf05e12788f22f15ff8023d7496df79
John <your-email@gmail.com>
2026-06-29 00:55:37 +0300

    Страница для стихов

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -36,6 +36,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/stihi`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: 'weekly' as const,
